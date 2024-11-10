5a93004144ccaebe28ef91157a5c8e9294aa301b
Slick McFavorite <keytonic@gmail.com>
2024-11-10 09:57:56 -0500

    commit

--- public/post.php
+++ public/post.php
@@ -8,7 +8,7 @@ $email = isset($_POST['email']) ? htmlspecialchars($_POST['email']) : "";
 $message = isset($_POST['message']) ? htmlspecialchars($_POST['message']) : "";
 
 $txt = wordwrap("Name: ".$name."\nPhone: ".$phone."\nEmail: ".$email."\nMessage: ".$message, 70, "\n");
-mail('keytonic@gmail.com', 'Portfolio Email', $txt, 'From: admin@andrewtowner.com' . "\r\n");
+mail('andrew@andrewtowner.com', 'Portfolio Email', $txt, 'From: admin@andrewtowner.com' . "\r\n");
 file_put_contents("email.log",$txt . "\n\n", FILE_APPEND);
 
 ?>
